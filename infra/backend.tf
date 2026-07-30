93cab0f914a3b5715f1d8cb355ed1157c23521d4
Yuri Carmo <y.carmo@ext.ppay.me>
2026-07-30 17:50:34 -0300

    fix(terraform): use unique S3 state bucket

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "togglemaster-tfstate-yuri-1"
+    bucket       = "togglemaster-tfstate-yuri-698173141574"
     key          = "eks-cluster/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = false # AWS ACADEMY: s3:PutObject no .tflock é bloqueado pela LabRole
