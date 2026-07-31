782a6fc977b63b1272145ca0a3abbf5cc062e01a
Yuri Carmo <y.carmo@ext.ppay.me>
2026-07-30 23:02:56 -0300

    fix(terraform): increase EKS node capacity

--- infra/modules/managed-node-group/mng.tf
+++ infra/modules/managed-node-group/mng.tf
@@ -8,8 +8,8 @@ resource "aws_eks_node_group" "eks_mng_nodegroup" {
   ]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 2
+    desired_size = 3
+    max_size     = 4
     min_size     = 2
   }
 
